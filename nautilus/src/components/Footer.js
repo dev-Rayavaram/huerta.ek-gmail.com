2a91d3c2b2fd5cdb69b0456ecaf017bacb3e29db
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-28 18:28:33 -0500

    changed font for footer

--- nautilus/src/components/Footer.js
+++ nautilus/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
     <div className="footer">
-            <h3><i>Channels</i></h3>
+            <h2><i>Channels</i></h2>
             <h5>TURN IN ON TO <i>NAUTILUS' </i> DEEP DIVE PORTALS</h5>
             
     </div>
