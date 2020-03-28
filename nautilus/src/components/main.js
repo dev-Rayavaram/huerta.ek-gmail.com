7f13a4e4d7c1103e98f6aead9aeb8d876e5a1c2b
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-27 19:11:26 -0500

    images added

--- nautilus/src/components/main.js
+++ nautilus/src/components/main.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import ai from '../assets/ai.png';
+import art from '../assets/art.png'
+import music from '../assets/music.png'
+
 
 function Main() {
     return (
-    <div class="header">
-            <h3 className="title">THE NAUTILUS SPOTLIGHT</h3>
-            <h1>WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
-            <button>READ NOW</button>
-      </div>
+        <div className="main">
+        <div className='leftbar'>
+            <div className='bar'>
+                <h3>ISSUE <small >079</small></h3>
+                <h1>CATALYST</h1>
+                <p style={{textAlign:"left"}}>What generates a new theory <br></br> in Science</p>
+            </div>  
+            <div className='bar'>
+                <p><b>CHAPTER ONE</b></p>
+                <p style={{textAlign:"left"}}>Art</p>
+            </div>  
+            <div className='bar'>
+                <p><b>CHAPTER TWO</b></p>
+                <p style={{textAlign:"left"}}>Mind</p>
+                <h4>Coming December 12</h4>
 
+            </div>  
+            <div className='bar'>
+                <p><b>CHAPTER THREE</b></p>
+                <p style={{textAlign:"left"}}>Energy</p>
+                <h4>Coming December 19</h4>
+
+            </div>  
+            <div className='bar'>
+                <p><b>CHAPTER FOUR</b></p>
+                <p style={{textAlign:"left"}}>Stars</p>
+                <h4>Coming December 26</h4>
+
+            </div>  
+
+         </div>
+         <div className='card'>
+             <div className='outer'>
+                <div className='image'>
+                    <img src={ai} alt='ai' style={{width:'100%', height:'100%'}}></img>
+                </div>
+             </div>
+             <div className='outer'>
+               </div>
+         </div>
+        </div>
     );
 };
     
