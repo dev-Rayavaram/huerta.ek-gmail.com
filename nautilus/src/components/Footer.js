8b1e2dd7fd80dfcc75558d04ab820a86a5b3a1fb
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-28 14:06:06 -0500

    added footer component

---
+++ nautilus/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+    <div className="footer">
+            <h3><i>Channels</i></h3>
+            <h5>TURN IN ON TO <i>NAUTILUS' </i> DEEP DIVE PORTALS</h5>
+            
+    </div>
+
+    );
+};
+    
+export default Footer;
