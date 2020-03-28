5fd9a6870dc9469f07d1ae76b3efef6dc26e83f7
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-28 17:32:01 -0500

    Header css changed

--- nautilus/src/components/Header.js
+++ nautilus/src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Header() {
     return (
-    <div class="header">
+    <div className="header">
             <h3 className="title">THE NAUTILUS SPOTLIGHT</h3>
-            <h1>WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
-            <button>READ NOW</button>
+            <h1 className='orange'>WHY THE LAWS OF PHYSICS ARE<br></br> INEVITABLE</h1>
+            <button className='button'>READ NOW</button>
       </div>
 
     );
